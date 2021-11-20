88491f71c4d0494e85fca94ecc153f85e93dd9db
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2021-11-20 00:20:23 -0300

    feat: search bar style done

---
+++ src/litepicker.js
@@ -0,0 +1,8 @@
+new Litepicker({
+    element: document.getElementById('date-start'),
+    elementEnd: document.getElementById('date-end'),
+    singleMode: false,
+    allowRepick: true,
+    numberOfColumns: 2,
+    numberOfMonths: 2
+  })
